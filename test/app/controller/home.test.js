08420b5b405917024f69ce6548593db249bcf9ba
zhangfeng <zhangfeng@goumin.com>
2020-04-01 12:06:27 +0800

    update test

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -1,20 +1,21 @@
-'use strict';
+"use strict";
 
-const { app, assert } = require('egg-mock/bootstrap');
-
-describe('test/app/controller/home.test.js', () => {
-  it('should assert', () => {
-    const pkg = require('../../../package.json');
+const { app, assert } = require("egg-mock/bootstrap");
+console.log(app);
+describe("test/app/controller/home.test.js", () => {
+  it("should assert", () => {
+    const pkg = require("../../../package.json");
     assert(app.config.keys.startsWith(pkg.name));
 
     // const ctx = app.mockContext({});
     // yield ctx.service.xx();
   });
 
-  it('should GET /', () => {
-    return app.httpRequest()
-      .get('/')
-      .expect('hi, egg')
+  it("should GET /", () => {
+    return app
+      .httpRequest()
+      .get("/")
+      .expect("hi, egg")
       .expect(200);
   });
 });
