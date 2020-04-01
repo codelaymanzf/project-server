4f7ed28997393b4e1b2a401a2f9dd15445f9e0e1
zhangfeng <zhangfeng@goumin.com>
2020-04-01 14:53:11 +0800

    init project

--- app/router.js
+++ app/router.js
@@ -1,10 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
   const { router, controller } = app;
-  console.log(router);
-  router.get('/', controller.home.index);
+  router.get("/", controller.home.index);
 };
