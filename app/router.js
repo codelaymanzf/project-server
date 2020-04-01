cda45af34fad7065b98b30f643efa889149692e2
zhangfeng <zhangfeng@goumin.com>
2020-04-01 12:03:46 +0800

    chore(router): add log
    n

--- app/router.js
+++ app/router.js
@@ -1,9 +1,10 @@
-'use strict';
+"use strict";
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  console.log(app);
+  router.get("/", controller.home.index);
 };
